b4125a06beaf9e39cdc16415f073db95136483b3
kaberege <kabgnestor@gmail.com>
2025-05-26 18:11:25 +0200

    added basic routing

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
+
+function Posts() {
+    return (
+        <div>
+            <Header />
+            <Footer />
+        </div>
+    )
+}
+
+export default Posts;
